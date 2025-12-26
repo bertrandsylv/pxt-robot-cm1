e51b5cc548c35e8e6af9a3d5aefccf123a888eb3
Sylvain Bertrand <bertrand_sylv@yahoo.fr>
2025-12-26 17:55:38 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
-function vitesse_droite_lent () {
+namespace robot {
+export function vitesse_droite_lent () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
 }
-function vitesse_droite_rapide () {
+export function vitesse_droite_rapide () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
 }
-function vitesse_gauche_rapide () {
+export function vitesse_gauche_rapide () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
 }
-function vitesse_gauche_lent () {
+export function vitesse_gauche_lent () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
+}
 }
