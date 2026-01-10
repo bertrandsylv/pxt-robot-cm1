0869a3d1f004833d68d2c5109c55d64febb8ddd1
sbertran <sylvain.bertrand@onera.fr>
2026-01-10 15:26:05 +0100

    .

--- robot.ts
+++ robot.ts
@@ -4,13 +4,14 @@ namespace robot {
 
 
 //% block="Roue droite vitesse lente"
-//%blockId="roue_droite_lent"
+//%blockId="robot_roue_droite_lent"
 //% weight=99
 export function vitesseDroiteLent () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
 }
 
 //% block="Roue droite vitesse rapide"
+//%blockId="robot_roue_droite_rapide"
 //% weight=98
 export function vitesseDroiteRapide () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
@@ -18,6 +19,7 @@ export function vitesseDroiteRapide () {
 
 
 //% block="Roue gauche vitesse lente"
+//%blockId="robot_roue_gauche_lent"
 //% weight=96
 export function vitesseGaucheLent () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
@@ -25,6 +27,7 @@ export function vitesseGaucheLent () {
 
 
 //% block="Roue gauche vitesse rapide"
+//%blockId="robot_roue_gauche_rapide"
 //% weight=97
 export function vitesseGaucheRapide () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
