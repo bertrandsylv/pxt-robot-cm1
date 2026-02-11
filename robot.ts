02fc10e6a685cf56dc1a43d4184d6d870973671a
sbertran <sylvain.bertrand@onera.fr>
2026-02-11 08:55:09 +0100

    tourner et reorganisation

--- robot.ts
+++ robot.ts
@@ -2,60 +2,9 @@
 //% weight=100 color=#5b3fe8" color=#f44242 icon="\uf185"
 namespace robot {
 
-	//% block="Faire tourner roue droite lentement"
-	//% blockId="robot_roue_droite_lent"
-	//% weight=99
-	export function vitesseDroiteLent () {
-		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
-	}
-
-	//% block="Faire tourner roue droite rapidement"
-	//% blockId="robot_roue_droite_rapide"
-	//% weight=98
-	export function vitesseDroiteRapide () {
-		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-	}
-
-
-	//% block="Faire tourner roue gauche lentement"
-	//% blockId="robot_roue_gauche_lent"
-	//% weight=97
-	export function vitesseGaucheLent () {
-		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
-	}
-
-
-	//% block="Faire tourner roue gauche rapidement"
-	//% blockId="robot_roue_gauche_rapide"
-	//% weight=96
-	export function vitesseGaucheRapide () {
-		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-	}
-	
-	
-	//% block="Arreter robot"
-	//% blockId="robot_arreter"
-	//% weight=95
-	export function arreterRobot () {
-		maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.LeftMotor)
-		maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.RightMotor)
-	}
-	
-	
-	
-
-	//% block="Avancer de |%distance cm"
-	//% blockId="robot_avancer"
-	//% weight=94
-	//% distance.defl=10
-	export function avancerRobot(distance:number){
-		maqueenPlusV2.pidControlDistance(maqueenPlusV2.SpeedDirection.SpeedCW, distance, maqueenPlusV2.MyInterruption.NotAllowed)
-		maqueenPlusV2.pidControlStop()
-	}
-	
 	//% block="Initialiser robot"
 	//% blockId="robot_initialiser"
-	//% weight=93
+	//% weight=99
 	export function initialiserRobot(){
 		maqueenPlusV2.I2CInit()
 		basic.clearScreen()
@@ -67,23 +16,74 @@ namespace robot {
 		}
 	}
 	
+	//% block="Avancer de |%distance cm"
+	//% blockId="robot_avancer"
+	//% weight=98
+	//% distance.defl=10
+	export function avancerRobot(distance:number){
+		maqueenPlusV2.pidControlDistance(maqueenPlusV2.SpeedDirection.SpeedCW, distance, maqueenPlusV2.MyInterruption.NotAllowed)
+		maqueenPlusV2.pidControlStop()
+	}
 	
 	
 	//% block="Tourner gauche"
 	//% blockId="robot_tourner_gauche"
-	//% weight=92
+	//% weight=97
 	export function tournerGaucheRobot(){
 		maqueenPlusV2.pidControlAngle(90, maqueenPlusV2.MyInterruption.NotAllowed)
 		maqueenPlusV2.pidControlStop()
 	}
-		
-
+	
+	
 	//% block="Tourner droite"
 	//% blockId="robot_tourner_droite"
-	//% weight=92
+	//% weight=96
 	export function tournerDroiteRobot(){
 		maqueenPlusV2.pidControlAngle(-90, maqueenPlusV2.MyInterruption.NotAllowed)
 		maqueenPlusV2.pidControlStop()
 	}
+
+	
+	
+	//% block="Arreter robot"
+	//% blockId="robot_arreter"
+	//% weight=95
+	export function arreterRobot () {
+		maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.LeftMotor)
+		maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.RightMotor)
+	}
+	
+
+
+	//% block="Faire tourner roue droite lentement"
+	//% blockId="robot_roue_droite_lent"
+	//% weight=79
+	export function vitesseDroiteLent () {
+		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
+	}
+
+	//% block="Faire tourner roue droite rapidement"
+	//% blockId="robot_roue_droite_rapide"
+	//% weight=78
+	export function vitesseDroiteRapide () {
+		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
+	}
+
+
+	//% block="Faire tourner roue gauche lentement"
+	//% blockId="robot_roue_gauche_lent"
+	//% weight=77
+	export function vitesseGaucheLent () {
+		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 30)
+	}
+
+
+	//% block="Faire tourner roue gauche rapidement"
+	//% blockId="robot_roue_gauche_rapide"
+	//% weight=76
+	export function vitesseGaucheRapide () {
+		maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
+	}
+
 
 }
